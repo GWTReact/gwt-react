1855e5499501ab84726204fa843b2fa9bad18c2d
Paul Stockley <pstockley1@gmail.com>
2016-06-28 23:17:58 -0400

    Upgrade to Mobx 2.3.2

--- dist/src/index-react-mobx.js
+++ dist/src/index-react-mobx.js
@@ -9,6 +9,7 @@ import ReactDOMServer from "react-dom/server";
 //If you don't MobX comment out the following line
 import * as MobX from "mobx";
 import * as MobXReact from "mobx-react";
+import * as MobXDevTools from "mobx-react-devtools";
 
 //Export the base React packages to the Window object for GWT to access
 window.React = React;
@@ -20,6 +21,7 @@ window.ReactDOMServer = ReactDOMServer;
 //If you don't use MobX comment out the following lines
 window.MobX = MobX;
 window.MobXReact = MobXReact;
+window.MobXDevTools = MobXDevTools;
 
 //*****************************************************************************
 // Support JS required for GWT React
