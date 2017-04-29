008e562c30046c06989886b3ed7e0f5ccbf9aa7e
Paul Stockley <pstockley1@gmail.com>
2017-04-29 07:45:34 -0400

    Make use of new JsConstructor available in GWT 2.8.1

--- src/gwt/react/client/components/ComponentUtils.java
+++ src/gwt/react/client/components/ComponentUtils.java
@@ -1,8 +1,6 @@
 package gwt.react.client.components;
 
-import gwt.interop.utils.client.collections.JsStringMap;
 import gwt.interop.utils.client.plainobjects.JsPlainObj;
-import gwt.interop.utils.shared.collections.StringMap;
 import gwt.react.client.proptypes.BaseProps;
 
 /**
@@ -12,8 +10,6 @@ public class ComponentUtils {
     private ComponentUtils() {
     }
 
-    private static StringMap<ComponentConstructorFn> constructorLookup = JsStringMap.create() ;
-
     /**
      * Given the Class of a JsType annotated {@link Component} class, return the constructor function to use in Javascript
      * @param cls The Class
@@ -22,39 +18,7 @@ public class ComponentUtils {
      * @param <T> The type of {@link Component}
      * @return The constructor function
      */
-    public static <P extends BaseProps, S extends JsPlainObj, T extends Component<P, S>> ComponentConstructorFn<P> getCtorFn(Class<T> cls) {
-        return getCtorFn(cls, cls.getName());
-    }
-
-    /**
-     * Given the fully qualified name of a JsType annotated {@link Component} class, return the constructor function to use in Javascript
-     * @param cls The Class
-     * @param actualClassName The fully qualified name. This is useful if it does not match the className of cls.
-     * @param <P>
-     * @return The constructor function
-     */
-    public static <P extends BaseProps, S extends JsPlainObj, T extends Component<P, S>> ComponentConstructorFn<P> getCtorFn(Class<T> cls, String actualClassName) {
-        ComponentConstructorFn<P> fn = constructorLookup.get(actualClassName);
-
-        if (fn == null) {
-            fn = getJSConstructorFn(actualClassName);
-            assert(fn != null);
-
-            constructorLookup.put(actualClassName, fn);
-        }
-        return fn;
-    }
-
-    /**
-     * TODO find a more efficient way of obtaining the constructor function
-     */
-    private static native <P extends BaseProps> ComponentConstructorFn<P> getJSConstructorFn(String className) /*-{
-        var namespaces = className.split(".");
-        var context = $wnd;
-
-        for (var i = 0; i < namespaces.length; i++) {
-            context = context[namespaces[i]];
-        }
-        return context;
-	}-*/;
+    public static native <P extends BaseProps, S extends JsPlainObj, T extends Component<P, S>> ComponentConstructorFn<P> getCtorFn(Class<T> cls) /*-{
+        return cls.@java.lang.Class::jsConstructor;
+    }-*/;
 }
