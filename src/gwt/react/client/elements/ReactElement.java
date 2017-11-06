7f34bdf0a9adcb11a82d696b0a9b97536c45fbfc
Ivan Markov <ivan.markov@honeywell.com>
2017-11-06 11:37:45 +0200

    #13

--- src/gwt/react/client/elements/ReactElement.java
+++ src/gwt/react/client/elements/ReactElement.java
@@ -1,9 +1,10 @@
 package gwt.react.client.elements;
 
 import gwt.react.client.proptypes.BaseProps;
+import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
-@JsType(isNative = true)
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
 public class ReactElement<P extends BaseProps, T> {
     public T type;
     public P props;
