a155417e6c21a7f224ca275d5a02676d4d4cb80f
pstockley <pstockley@onecornerstone.com>
2018-05-02 13:20:16 -0400

    Implementing the builder pattern for Props classes requires too much ceremony and does not play well with inheritance #16

--- src/gwt/react/client/proptypes/html/LabelProps.java
+++ src/gwt/react/client/proptypes/html/LabelProps.java
@@ -15,7 +15,7 @@ import jsinterop.annotations.JsType;
 @JsType(isNative=true, namespace=JsPackage.GLOBAL, name="Object")
 public class LabelProps extends HtmlGlobalFields {
 
-    @JsOverlay public final LabelProps HtmlFor(String s) { htmlFor = s; return this; }
+    @JsOverlay public final LabelProps htmlFor(String s) { htmlFor = s; return this; }
     @JsOverlay public final LabelProps form(String s) { form = s; return this; }
 
     //React Specific
@@ -25,55 +25,55 @@ public class LabelProps extends HtmlGlobalFields {
     @JsOverlay public final LabelProps key(String s) { key = s; return this; }
 
     //Global HTML props
-    @JsOverlay public final LabelProps AccessKey(String s) { accessKey = s; return this;}
-    @JsOverlay public final LabelProps ClassName(String s) { className = s; return this; }
-    @JsOverlay public final LabelProps ContentEditable(boolean b) { contentEditable = b; return this; }
-    @JsOverlay public final LabelProps ContextMenu(String s) { contextMenu = s; return this; }
-    @JsOverlay public final LabelProps Dir(String s) { dir = s; return this; }
-    @JsOverlay public final LabelProps Draggable(boolean b) { draggable = b; return this; }
-    @JsOverlay public final LabelProps Hidden(boolean b) { hidden = b; return this; }
-    @JsOverlay public final LabelProps Id(String s) { id = s; return this; }
-    @JsOverlay public final LabelProps Lang(String s) { lang = s; return this; }
-    @JsOverlay public final LabelProps Spellcheck(boolean b) { spellCheck = b; return this; }
-    @JsOverlay public final LabelProps Style(CssProps s) { style = s; return this; }
-    @JsOverlay public final LabelProps TabIndex(int i) { tabIndex = i; return this; }
-    @JsOverlay public final LabelProps Title(String s) { title = s; return this; }
-    @JsOverlay public final LabelProps Translate(YesNo s) { translate = s.name(); return this; }
+    @JsOverlay public final LabelProps accessKey(String s) { accessKey = s; return this;}
+    @JsOverlay public final LabelProps className(String s) { className = s; return this; }
+    @JsOverlay public final LabelProps contentEditable(boolean b) { contentEditable = b; return this; }
+    @JsOverlay public final LabelProps contextMenu(String s) { contextMenu = s; return this; }
+    @JsOverlay public final LabelProps dir(String s) { dir = s; return this; }
+    @JsOverlay public final LabelProps draggable(boolean b) { draggable = b; return this; }
+    @JsOverlay public final LabelProps hidden(boolean b) { hidden = b; return this; }
+    @JsOverlay public final LabelProps id(String s) { id = s; return this; }
+    @JsOverlay public final LabelProps lang(String s) { lang = s; return this; }
+    @JsOverlay public final LabelProps spellcheck(boolean b) { spellCheck = b; return this; }
+    @JsOverlay public final LabelProps style(CssProps s) { style = s; return this; }
+    @JsOverlay public final LabelProps tabIndex(int i) { tabIndex = i; return this; }
+    @JsOverlay public final LabelProps title(String s) { title = s; return this; }
+    @JsOverlay public final LabelProps translate(YesNo s) { translate = s.name(); return this; }
 
     //Applicable Event Handlers
 
     //TODO Refine
     // Focus Events
-    @JsOverlay public final LabelProps OnBlur(FocusEventHandler handler) { onBlur = handler; return this; }
-    @JsOverlay public final LabelProps OnFocus(FocusEventHandler handler) { onFocus = handler; return this; }
+    @JsOverlay public final LabelProps onBlur(FocusEventHandler handler) { onBlur = handler; return this; }
+    @JsOverlay public final LabelProps onFocus(FocusEventHandler handler) { onFocus = handler; return this; }
 
     // Keyboard Events
-    @JsOverlay public final LabelProps OnKeyDown(KeyboardEventHandler handler) { onKeyDown = handler; return this; }
-    @JsOverlay public final LabelProps OnKeyPress(KeyboardEventHandler handler) { onKeyPress = handler; return this; }
-    @JsOverlay public final LabelProps OnKeyUp(KeyboardEventHandler handler) { onKeyUp = handler; return this; }
+    @JsOverlay public final LabelProps onKeyDown(KeyboardEventHandler handler) { onKeyDown = handler; return this; }
+    @JsOverlay public final LabelProps onKeyPress(KeyboardEventHandler handler) { onKeyPress = handler; return this; }
+    @JsOverlay public final LabelProps onKeyUp(KeyboardEventHandler handler) { onKeyUp = handler; return this; }
 
-    @JsOverlay public final LabelProps OnClick(MouseEventHandler handler) { onClick = handler; return this; }
-    @JsOverlay public final LabelProps OnContextMenu(MouseEventHandler handler) { onContextMenu = handler; return this; }
-    @JsOverlay public final LabelProps OnDoubleClick(MouseEventHandler handler) { onDoubleClick = handler; return this; }
-    @JsOverlay public final LabelProps OnDrag(DragEventHandler handler) { onDrag = handler; return this; }
-    @JsOverlay public final LabelProps OnDragEnd(DragEventHandler handler) { onDragEnd = handler; return this; }
-    @JsOverlay public final LabelProps OnDragEnter(DragEventHandler handler) { onDragEnter = handler; return this; }
-    @JsOverlay public final LabelProps OnDragExit(DragEventHandler handler) { onDragExit = handler; return this; }
-    @JsOverlay public final LabelProps OnDragLeave(DragEventHandler handler) { onDragLeave = handler; return this; }
-    @JsOverlay public final LabelProps OnDragOver(DragEventHandler handler) { onDragOver = handler; return this; }
-    @JsOverlay public final LabelProps OnDragStart(DragEventHandler handler) { onDragStart = handler; return this; }
-    @JsOverlay public final LabelProps OnDrop(DragEventHandler handler) { onDrop = handler; return this; }
-    @JsOverlay public final LabelProps OnMouseDown(MouseEventHandler handler) { onMouseDown = handler; return this; }
-    @JsOverlay public final LabelProps OnMouseEnter(MouseEventHandler handler) { onMouseEnter = handler; return this; }
-    @JsOverlay public final LabelProps OnMouseLeave(MouseEventHandler handler) { onMouseLeave = handler; return this; }
-    @JsOverlay public final LabelProps OnMouseMove(MouseEventHandler handler) { onMouseMove = handler; return this; }
-    @JsOverlay public final LabelProps OnMouseOut(MouseEventHandler handler) { onMouseOut = handler; return this; }
-    @JsOverlay public final LabelProps OnMouseOver(MouseEventHandler handler) { onMouseOver = handler; return this; }
-    @JsOverlay public final LabelProps OnMouseUp(MouseEventHandler handler) { onMouseUp = handler; return this; }
+    @JsOverlay public final LabelProps onClick(MouseEventHandler handler) { onClick = handler; return this; }
+    @JsOverlay public final LabelProps onContextMenu(MouseEventHandler handler) { onContextMenu = handler; return this; }
+    @JsOverlay public final LabelProps onDoubleClick(MouseEventHandler handler) { onDoubleClick = handler; return this; }
+    @JsOverlay public final LabelProps onDrag(DragEventHandler handler) { onDrag = handler; return this; }
+    @JsOverlay public final LabelProps onDragEnd(DragEventHandler handler) { onDragEnd = handler; return this; }
+    @JsOverlay public final LabelProps onDragEnter(DragEventHandler handler) { onDragEnter = handler; return this; }
+    @JsOverlay public final LabelProps onDragExit(DragEventHandler handler) { onDragExit = handler; return this; }
+    @JsOverlay public final LabelProps onDragLeave(DragEventHandler handler) { onDragLeave = handler; return this; }
+    @JsOverlay public final LabelProps onDragOver(DragEventHandler handler) { onDragOver = handler; return this; }
+    @JsOverlay public final LabelProps onDragStart(DragEventHandler handler) { onDragStart = handler; return this; }
+    @JsOverlay public final LabelProps onDrop(DragEventHandler handler) { onDrop = handler; return this; }
+    @JsOverlay public final LabelProps onMouseDown(MouseEventHandler handler) { onMouseDown = handler; return this; }
+    @JsOverlay public final LabelProps onMouseEnter(MouseEventHandler handler) { onMouseEnter = handler; return this; }
+    @JsOverlay public final LabelProps onMouseLeave(MouseEventHandler handler) { onMouseLeave = handler; return this; }
+    @JsOverlay public final LabelProps onMouseMove(MouseEventHandler handler) { onMouseMove = handler; return this; }
+    @JsOverlay public final LabelProps onMouseOut(MouseEventHandler handler) { onMouseOut = handler; return this; }
+    @JsOverlay public final LabelProps onMouseOver(MouseEventHandler handler) { onMouseOver = handler; return this; }
+    @JsOverlay public final LabelProps onMouseUp(MouseEventHandler handler) { onMouseUp = handler; return this; }
 
     // Touch Events
-    @JsOverlay public final LabelProps OnTouchCancel(TouchEventHandler handler) { onTouchCancel = handler; return this; }
-    @JsOverlay public final LabelProps OnTouchEnd(TouchEventHandler handler) { onTouchEnd = handler; return this; }
-    @JsOverlay public final LabelProps OnTouchMove(TouchEventHandler handler) { onTouchMove = handler; return this; }
-    @JsOverlay public final LabelProps OnTouchStart(TouchEventHandler handler) { onTouchStart = handler; return this; }
+    @JsOverlay public final LabelProps onTouchCancel(TouchEventHandler handler) { onTouchCancel = handler; return this; }
+    @JsOverlay public final LabelProps onTouchEnd(TouchEventHandler handler) { onTouchEnd = handler; return this; }
+    @JsOverlay public final LabelProps onTouchMove(TouchEventHandler handler) { onTouchMove = handler; return this; }
+    @JsOverlay public final LabelProps onTouchStart(TouchEventHandler handler) { onTouchStart = handler; return this; }
 }
